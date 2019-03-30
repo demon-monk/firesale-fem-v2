d9f3806963a94130b7b9bcdfbe9adbb799f25894
demon-monk <antdude@126.com>
2019-03-31 00:14:00 +0800

    feat: setup main process and render process

--- app/main.js
+++ app/main.js
@@ -1,1 +1,12 @@
-console.log('This is an Electron application!');
+const { app, BrowserWindow } = require('electron');
+const path = require('path')
+
+let mainWindow = null;
+app.on('ready', () => {
+    /** show window once after html is loaded, in case a blink of blank at startup **/
+    mainWindow = new BrowserWindow({ show: false })
+    mainWindow.loadFile(path.join(__dirname, 'index.html'))
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
+})
