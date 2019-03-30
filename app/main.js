7426a2d6cba136af94beb1bf0115488588d43388
demon-monk <antdude@126.com>
2019-03-31 00:31:19 +0800

    feat: access file system

--- app/main.js
+++ app/main.js
@@ -1,11 +1,26 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path')
+const fs = require('fs')
 
 let mainWindow = null;
+const getFileFromUser = () => {
+    const filenames = dialog.showOpenDialog({
+        properties: ['openFile'],
+        filters: [
+            { name: 'Text Files', extensions: ['txt', 'text']},
+            { name: 'Markdown Files', extensions: ['md', 'markdown']},
+        ],
+    })
+    if(!filenames) return;
+    const filename = filenames[0]
+    const content = fs.readFileSync(filename).toString()
+    console.log(content);
+}
 app.on('ready', () => {
     /** show window once after html is loaded, in case a blink of blank at startup **/
     mainWindow = new BrowserWindow({ show: false })
     mainWindow.loadFile(path.join(__dirname, 'index.html'))
+    getFileFromUser();
     mainWindow.once('ready-to-show', () => {
         mainWindow.show()
     })
